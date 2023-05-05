1876cc5200ae52fc1a176dcd95ce923e89f9757a
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-05-04 23:14:54 -0300

    multiples address working

--- src/pages/components/FormStep2.jsx
+++ src/pages/components/FormStep2.jsx
@@ -2,12 +2,13 @@ import styles from '../../styles/FormSteps.module.css'
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Autocomplete from '@mui/material/Autocomplete';
-import Divider from '@mui/material/Divider'
+import Box from '@mui/material/Box';
 import React, {useState, useEffect} from 'react'
 
 export default function FormStep2() {
 
-  const [amoutAddresses, setAmoutAddresses] = useState(1)
+  const [amoutAddresses, setAmoutAddresses] = useState(0)
+  const [currentAddress, setCurrentAddress] = useState(0);
   const states = [
     'Acre',
     'Alagoas',
@@ -41,60 +42,67 @@ export default function FormStep2() {
   return (
     <>
       <div className={styles.formContainer}>
-        {Array.from({ length: amoutAddresses }).map((_, index) =>(
+        <Box sx={{display: 'flex', mb: '10px'}}>
+          {Array.from({ length: amoutAddresses + 1}).map((_, index) =>(
+              <Typography variant='subtitle1' sx={{width: 'max-content', mr: '26px', ml: '5px', transition: '0.3s', textDecoration: 'underline', cursor: 'pointer',  ':hover': {transform: 'scale(1.05)', transition: '0.1s'}}} key={index} onClick={() => setCurrentAddress(index)}>Endereço {index + 1}</Typography>
+          ))}
+        </Box>
+          
+        {Array.from({ length: amoutAddresses + 1}).map((_, index) => (
           <div key={index}>
-            <div className={styles.inputContainer} key={index}>
-              <div className={styles.inputs}>
-                  <Typography variant='subtitle1'>CEP</Typography>
-                  <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-                </div>
-                <div className={styles.inputs}>
-                  <Typography variant='subtitle1'>Nome da Rua</Typography>
-                  <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-                </div>
-            </div>
-
-            <div className={styles.inputContainer}>
-              <div className={styles.inputs}>
-                <Typography variant='subtitle1'>Número</Typography>
-                <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-              </div>
-              <div className={styles.inputs}>
-                <Typography variant='subtitle1'>Complemento</Typography>
-                <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-              </div>
-              <div className={styles.inputs}>
-                <Typography variant='subtitle1'>Bairro</Typography>
-                <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-              </div>
-            </div>
-
-            <div className={styles.inputContainer}>
-              <div className={styles.inputs}>
-                <Typography variant='subtitle1'>Estado</Typography>
-                <Autocomplete
-                  size="small"
-                  id="disabled-options-demo"
-                  options={states}
-                  renderInput={(params) => <TextField {...params} label="" />}
-                />
-                <div className={styles.emptyMessageContainer}> 
-                  {/* <Typography variant='subtitle2'>O campo "Seu telefone" é obrigatório</Typography> */}
-                </div>
-              </div>
-              <div className={styles.inputs}>
-                <Typography variant='subtitle1'>Cidade</Typography>
-                <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
-              </div>
-            </div>
-            {amoutAddresses >= 2 ? (
-              <>
-                <Divider sx={{my: '10px'}}/>
-              </>
-            ):null}
+              {index === currentAddress ? (
+                <>
+                  bla{index + 1}
+                </>
+              ) : null}
           </div>
         ))}
-        <Typography variant='subtitle1' sx={{textDecoration: 'underline' , margin: '0 auto', cursor: 'pointer', width: 'max-content', px: '20px', py: '8px', mt: '-10px', transition: '0.3s',  ':hover': {transform: 'scale(1.05)', transition: '0.3s'},}} onClick={() => setAmoutAddresses(amoutAddresses + 1)}>
+
+        <div className={styles.inputContainer} >
+          <div className={styles.inputs}>
+              <Typography variant='subtitle1'>CEP</Typography>
+              <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+            </div>
+            <div className={styles.inputs}>
+              <Typography variant='subtitle1'>Nome da Rua</Typography>
+              <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+            </div>
+        </div>
+
+        <div className={styles.inputContainer}>
+          <div className={styles.inputs}>
+            <Typography variant='subtitle1'>Número</Typography>
+            <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+          </div>
+          <div className={styles.inputs}>
+            <Typography variant='subtitle1'>Complemento</Typography>
+            <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+          </div>
+          <div className={styles.inputs}>
+            <Typography variant='subtitle1'>Bairro</Typography>
+            <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+          </div>
+        </div>
+
+        <div className={styles.inputContainer}>
+          <div className={styles.inputs}>
+            <Typography variant='subtitle1'>Estado</Typography>
+            <Autocomplete
+              size="small"
+              id="disabled-options-demo"
+              options={states}
+              renderInput={(params) => <TextField {...params} label="" />}
+            />
+             <div className={styles.emptyMessageContainer}> 
+              {/* <Typography variant='subtitle2'>O campo "Seu telefone" é obrigatório</Typography> */}
+            </div>
+          </div>
+          <div className={styles.inputs}>
+            <Typography variant='subtitle1'>Cidade</Typography>
+            <TextField size="small" id="outlined-basic" variant="outlined" fullWidth/>
+          </div>
+        </div>
+        <Typography variant='subtitle1' sx={{textDecoration: 'underline' , margin: '0 auto', cursor: 'pointer', width: 'max-content', px: '20px', py: '5px', mt: '-12px', transition: '0.3s',  ':hover': {transform: 'scale(1.05)', transition: '0.3s'}}} onClick={() => setAmoutAddresses(amoutAddresses + 1)}>
           Adicionar mais um endereço (opcional)
         </Typography>
       </div>
